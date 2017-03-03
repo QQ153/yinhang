33961631f955515690130231de16a9aae4063eef
qq153 <1534433439@qq.com>
2017-03-03 11:45:30 +0800

    6

--- js/index.js
+++ js/index.js
@@ -3,13 +3,13 @@ var ye = document.getElementById("yuer").innerHTML;
 var num = document.getElementById("num");
 var reg = /^\s+|\s+$/g;
 
-num.onkeydown = function(e){
-	console.log(e);
-	var e = e||window.event;
-            if(!((e.keyCode >= 48 && e.keyCode <= 57)||(e.keyCode >=96 && e.keyCode <=105))){
-                e.returnValue=false; 
-            }
-        }
+//num.onkeydown = function(e){
+//	console.log(e);
+//	var e = e||window.event;
+//          if(!((e.keyCode >= 48 && e.keyCode <= 57)||(e.keyCode >=96 && e.keyCode <=105))){
+//              e.returnValue=false; 
+//          }
+//      }
 tx.onclick = function(){
 	if(num.value){
 		if(num.value<ye){
